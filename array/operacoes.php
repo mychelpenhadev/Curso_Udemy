550be5b925bec97486aafcb119f9ac99e7d70504
mychelpenhadev <mychelcajari@gmail.com>
2025-05-16 00:28:15 -0300

    operacoes

--- array/operacoes.php
+++ array/operacoes.php
@@ -1,3 +1,14 @@
 <div class="titulo">Operações</div>
 
 <?php
+$dados1 = [
+    "nome =" => "Jose",
+    "idade" => 28
+];
+
+$dados2 = [
+    "naturalidade" => "Fortaleza"
+];
+
+$dadosCompletos = $dados1 + $dados2;
+print_r($dadosCompletos);
